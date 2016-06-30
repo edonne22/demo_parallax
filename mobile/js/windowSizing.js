2753540233810a0b5ccacfe3ae768ddfa2530966
Eliza Donne <eliza.donne@gmail.com>
2016-06-30 15:13:21 -0400

    detect orientation change

--- mobile/js/windowSizing.js
+++ mobile/js/windowSizing.js
@@ -2,7 +2,16 @@ function windowResize(){
 
 	//resize for landscape
 	if( $(window).innerWidth() <= 320 ){
-		$( ".content").css( "width", "320px" );
+		$( "body").css( "width", "320px" );
+		$( "body").css( "height", "568px" );
+		$(".content").css("width","320px");
+		$(".content").css("height","568px");
+		$( "#header").css( "width","320px" );
+		$( "#hamburger").css( "margin-top","-65px" );
+		$( "#slide1").css( "background-size", "320px 568px" );
+		$( "#slide1").css( "height", "568px" );
+		$( "#slide1").css( "padding-top", "185px" );
+		$( ".text_container").css( "width","260px" );
 	}
 	if( $(window).innerWidth() >= 480){
 		$( "body").css( "width", "568px" );
