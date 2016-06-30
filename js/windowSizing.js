a6e773596e5061ab182b456125ad2168a88c3409
Eliza Donne <eliza.donne@gmail.com>
2016-06-30 14:58:19 -0400

    mobile layout changes

--- js/windowSizing.js
+++ js/windowSizing.js
@@ -1,3 +1,14 @@
+// Re-direct the user to the Mobile site if using an iPhone/iPod/Android
+if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)))
+{
+	location.replace("mobile/mobile.html");
+}
+else if((navigator.userAgent.match(/Android/i)))
+{
+	location.replace("mobile/mobile.html");
+}
+
+
 function windowResize(){
 
 	//resize for iPad
--- mobile/js/windowSizing.js
+++ mobile/js/windowSizing.js
@@ -1,9 +1,19 @@
 function windowResize(){
 
-	//resize for iPad
-	if( $(window).innerWidth() <= 1024 ){
-		$( "#slide1").css( "background-size", "1024px 900px" );
-		$( "#slide3").css( "background-size", "100% 65%" );
+	//resize for landscape
+	if( $(window).innerWidth() <= 320 ){
+		$( ".content").css( "width", "320px" );
 	}
-
+	if( $(window).innerWidth() >= 480){
+		$( "body").css( "width", "568px" );
+		$( "body").css( "height", "320px" );
+		$(".content").css("width","568px");
+		$(".content").css("height","320px");
+		$( "#header").css( "width","568px" );
+		$( "#hamburger").css( "margin-top","20px" );
+		$( "#slide1").css( "background-size", "568px 320px" );
+		$( "#slide1").css( "height", "320px" );
+		$( "#slide1").css( "padding-top", "120px" );
+		$( ".text_container").css( "width","450px" );
+	}
 }
